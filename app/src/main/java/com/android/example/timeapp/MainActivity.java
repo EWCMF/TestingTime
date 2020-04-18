bd28b39e0709e11275774e13e39da77c7ee51379
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-04-18 17:30:04 +0200

    Fixed daylight savings time.

--- app/src/main/java/com/android/example/timeapp/MainActivity.java
+++ app/src/main/java/com/android/example/timeapp/MainActivity.java
@@ -7,25 +7,28 @@ import android.widget.TextView;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 import org.threeten.bp.Year;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView1;
-    private TextView textView2;
-    private TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onStart() {
+        super.onStart();
         AndroidThreeTen.init(this);
         int year = Year.now().getValue();
 
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
-        int timeZoneOffset = gregorianCalendar.getTimeZone().getRawOffset() / 3600000;
+        TimeZone timeZone = TimeZone.getTimeZone("Europe/Copenhagen");
+        boolean daylightTime = timeZone.inDaylightTime(gregorianCalendar.getTime());
 
-        textView1 = findViewById(R.id.text1);
-        textView2 = findViewById(R.id.text2);
-        textView3 = findViewById(R.id.text3);
+        TextView textView1 = findViewById(R.id.text1);
+        TextView textView2 = findViewById(R.id.text2);
+        TextView textView3 = findViewById(R.id.text3);
 
         String numDaysYear;
         String weeksPerYear;
@@ -38,14 +41,12 @@ public class MainActivity extends AppCompatActivity {
             weeksPerYear = "/52";
         }
         String timeZoneName;
-        if (timeZoneOffset == 1) {
+        if (!daylightTime) {
             timeZoneName = "Central European Time (UTC+1)";
         }
-        else if (timeZoneOffset == 2) {
+        else {
             timeZoneName = "Central European Summer Time (UTC+2)";
         }
-        else
-            timeZoneName = "Outside Denmark not supported";
 
         textView1.setText("Day of the year: " + gregorianCalendar.get(GregorianCalendar.DAY_OF_YEAR) + numDaysYear);
         textView2.setText("Week of the year: " + gregorianCalendar.get(GregorianCalendar.WEEK_OF_YEAR) + weeksPerYear);
